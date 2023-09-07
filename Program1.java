a418643c3246054bd207721f22bee03b6ddde890
Kartiklongadge <129276939+Kartiklongadge@users.noreply.github.com>
2023-09-07 19:22:40 +0530

    My Patterns Program  Using Java

---
+++ Program1.java
@@ -0,0 +1,35 @@
+public class Program1{
+   public static void main(String[] args){     
+		int n=5;
+      		for(int i=1;i<=(n*2)-1;i++){
+			if(i<=n)
+				for(int j=i;j>=1;j--)
+				System.out.print("* ");
+			else
+				for(int j=i;j<=(n*2)-1;j++)
+				System.out.print("* ");
+			
+			System.out.println();
+
+
+			
+		}
+	}
+
+
+}
+//5 ko 9 banane ka formula --->((n*2)-1);
+/*
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+
+*/
